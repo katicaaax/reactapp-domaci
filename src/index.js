e948c5a6ba91b6fd0d2494304762f04319c0aaa7
katicaaax <radosavljevic.katarina98@gmail.com>
2023-04-26 23:08:01 +0200

    Inicijalizacija aplikacije. Prve komponente.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,26 @@ import React from "react"; //importujemo potrebne biblioteke
 import  ReactDOM, { render } from "react-dom";
 
 //pravimo prvi element
-const prviElement = <h1>Hello world</h1>;
+//const prviElement = <h1>Hello world</h1>;
+//ubacivanje varijable i konstante u funkciju
+
+function formatName(user){
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = { //objekat user
+  firstName: 'Katarina',
+  lastName: 'Radosavljevic'
+};
+
+const element = (
+  <h1>
+  Hello, {formatName(user)}!
+</h1>
+);
+
 
 ReactDOM.render(
-  prviElement,
+  element,
   document.getElementById("root")
 );
